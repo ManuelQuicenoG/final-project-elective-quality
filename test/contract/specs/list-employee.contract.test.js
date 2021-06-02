630613c413598a0b6cd008f2d79c5eca4c5a1d22
manuel.quiceno <manuel.quiceno@correo.icesi.edu.co>
2021-06-01 20:02:54 -0500

    commit pact

--- test/contract/specs/list-employee.contract.test.js
+++ test/contract/specs/list-employee.contract.test.js
@@ -1,28 +1,29 @@
-import {Matchers} from '@pact-foundation/pact';
-import {employeeController} from '../../../controllers';
-import {provider} from '../config/initPact';
+import { Matchers } from '@pact-foundation/pact';
+import { employeeController } from '../../../controllers';
+import { provider } from '../config/initPact';
 
 describe('Employee Service', () => {
+    beforeAll(async() => {
+        await provider.setup();
+    });
     describe('When a request to list all employees is made', () => {
         beforeAll(async () => {
-            await provider.setup();
             await provider.addInteraction({
                 uponReceiving: 'a request to list all employees',
                 state: "has employees",
                 withRequest: {
                     method: 'GET',
-                    path: '/employees'
+                    path: '/animals'
                 },
                 willRespondWith: {
                     status: 200,
                     body: Matchers.eachLike(
                         {
-                            name: Matchers.like('Manuel nuevo'),
+                            name: Matchers.like('manuel nuevo'),
                             rolPosition: Matchers.like("Administración"),
                             gender: Matchers.like("Male"),
-                            hasCourses: Matchers.boolean(true),
-                            vaccines: Matchers.eachLike({id:Matchers.like(1211),name:Matchers.like("Inducción")})
-                        }
+                            hasCourses: Matchers.boolean(true)
+                        }, {min: 1}
                     )
                 }
             });
@@ -35,6 +36,6 @@ describe('Employee Service', () => {
             await provider.verify()
         });
 
-        afterAll(() => provider.finalize());
+        afterAll(async() =>await provider.finalize());
     });
 });
